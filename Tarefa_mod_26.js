5afe2cc67fe210fbdd652b4f433dc6ed16aefba6
Miguel Custodio <126588181+Miguel-Custodio@users.noreply.github.com>
2023-06-29 07:27:16 -0300

    Executando tarefa modulo 26

---
+++ Tarefa_mod_26.js
@@ -0,0 +1,11 @@
+"use strict";
+function calcMulti(numA, numB) {
+    return numA * numB;
+}
+const resultadoCalcMulti = calcMulti(10, 20);
+console.log(resultadoCalcMulti);
+function dizOla(nomeUser) {
+    return "Olá" + nomeUser;
+}
+const resultadoDizOla = dizOla(" Miguel");
+console.log(resultadoDizOla);
---
+++ Tarefa_mod_26.ts
@@ -0,0 +1,14 @@
+//Função Multiplição
+function calcMulti (numA: number, numB:number): number {
+    return numA * numB;
+}
+const resultadoCalcMulti = calcMulti(10,20);
+console.log(resultadoCalcMulti);
+
+
+//função Saudação
+function dizOla (nomeUser: string): string {
+    return "Olá" + nomeUser;
+}
+const resultadoDizOla = dizOla(" Miguel")
+console.log(resultadoDizOla);
